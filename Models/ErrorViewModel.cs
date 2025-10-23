516deaefac88391a00c6c22116b7b58e420a0a6a
nic763 <nikishermandez@gmail.com>
2025-10-23 08:51:33 -0500

    Proyecto base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyecto_nicol.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
